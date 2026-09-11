a0ab2b65a5c1bba3d11102a6d48c6b70f85bfced
hnrijs <198484681+hnrijs@users.noreply.github.com>
2026-09-11 18:53:16 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,14 +14,15 @@ sudo pacman -S --needed --noconfirm \
   base-devel wget dolphin imv btop playerctl alacritty zip unzip polkit-kde-agent ark \
   wl-clipboard slurp grim ttf-jetbrains-mono-nerd noto-fonts-emoji ttf-nerd-fonts-symbols \
   gtk3 pavucontrol nwg-look mpv brightnessctl nano android-udev \
-  power-profiles-daemon python-gobject xdg-desktop-portal-hyprland hyprlock hypridle hyprpicker \
+  power-profiles-daemon python-gobject hyprland xdg-desktop-portal-hyprland hyprlock hypridle hyprpicker \
   lightdm lightdm-gtk-greeter dunst aria2 jdk-openjdk \
   curl jq xdg-utils libnotify librewolf imagemagick audacious kdegraphics-thumbnailers ffmpegthumbs \
   ttf-dejavu ttf-font-awesome noto-fonts monolith \
   noto-fonts-cjk udisks2 kio-extras dolphin-plugins redshift \
   signal-desktop obs-studio proton-vpn-gtk-app \
-  less neovim ripgrep fd lazygit fastfetch yt-dlp \
-  fzf ncdu python-requests exiftool speedtest-cli krita gimp nodejs npm
+  less neovim vim ripgrep fd lazygit fastfetch yt-dlp \
+  fzf ncdu python-requests exiftool speedtest-cli krita gimp nodejs npm \
+  iwd openssh qt5-wayland qt6-wayland smartmontools uwsm wireless_tools
 
 if [ -d "$SCRIPT_DIR/theme" ]; then
   sudo mkdir -p /usr/share/themes
@@ -64,3 +65,4 @@ sudo systemctl enable lightdm
 
 sleep 5
 sudo reboot
+
