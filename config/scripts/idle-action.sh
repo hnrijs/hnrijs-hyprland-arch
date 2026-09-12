81b3889d93ca8376d148a2f8a8f29bff17a30e28
hnrijs <198484681+hnrijs@users.noreply.github.com>
2026-09-13 01:57:41 +0300

    Delete config/scripts directory

--- config/scripts/idle-action.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-[[ ! -f ${XDG_RUNTIME_DIR:?}/hshell-caffeine ]] || exit 0
-case ${1:-} in
-    lock) loginctl lock-session ;;
-    suspend) systemctl suspend ;;
-    *) exit 2 ;;
-esac
--- config/scripts/system-update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-printf 'Full system update\n'
-if command -v paru >/dev/null; then paru -Syu
-elif command -v yay >/dev/null; then yay -Syu
-else sudo pacman -Syu
-fi
