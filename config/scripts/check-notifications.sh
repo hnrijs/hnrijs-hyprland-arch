06e27fcef52d34cce358e8220cca4d133d7b8202
hnrijs <198484681+hnrijs@users.noreply.github.com>
2026-09-13 21:41:36 +0300

    Delete config/scripts directory

--- config/scripts/check-notifications.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -u
-printf 'Notification server:\n'
-gdbus call --session --dest org.freedesktop.Notifications --object-path /org/freedesktop/Notifications --method org.freedesktop.Notifications.GetServerInformation
-printf '\nRunning hshell instances:\n'
-qs list 2>&1
-printf '\nSending test notification:\n'
-notify-send -a hshell -t 5000 'Notifications ready' 'hshell test'
